e39c9f90b58161d6c6f8d80d35a9a6ff494ad6ff
Sprite_Spirit <a.backer@yandex.com>
2022-05-13 12:35:34 +0300

    not working correctly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Employee employer;
+        employer = new Employee();
+        employer.addNameToList("John");
+        employer.setFirstName("John");
+        employer.setLastName("Martins");
+        employer.setPatronymic("P.");
+        employer.setDepartment(2);
+        employer.setSalary(72000.6f);
+
+        Employee employer2;
+        employer2 = new Employee();
+        employer.addNameToList("Kate");
+        employer.setFirstName("Kate");
+        employer.setLastName("Stevens");
+        employer.setPatronymic("G.");
+        employer.setDepartment(3);
+        employer.setSalary(63000.1f);
+
+        employer.getEmployeesCount();
+        employer.EmployeesCount();
+
+
+
     }
 }
